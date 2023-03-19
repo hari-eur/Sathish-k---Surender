569ac048ff3841e7f727c37ec2b9eb4203a619ec
Sathishkumar-eUR <125849714+Sathishkumar-eUR@users.noreply.github.com>
2023-03-19 14:45:48 +0530

    Update stringSorting.c

--- Exercise 3/stringSorting.c
+++ Exercise 3/stringSorting.c
@@ -1,20 +1,15 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
 char* sort(char str[]){
-    
     int n = strlen(str);
     char previous =  str[0];
     int start = 0, end = 0;
-    
     int i=1;
     while( i != n+1){
         char after = str[i];
-       
         if( ( (previous>='0' && previous<='9') && (after>='a' && after <= 'z') ) || ((previous>='a' && previous<='z') && (after>='0' && after <= '9')) || (i == n) ){
-            
             end = i;
             for(int m =  start ; m < end ; m++){
                 for(int n = m ; n < end  ; n++){
@@ -30,19 +25,13 @@ char* sort(char str[]){
         }
         i++;
     }
-    
-    
     return str;
-    
 }
 
 int main()
 {
    char str[100];
     scanf("%s", str);
-    
     char* ans = sort(str);
     printf("%s\n",ans);
-    
-   
 }
